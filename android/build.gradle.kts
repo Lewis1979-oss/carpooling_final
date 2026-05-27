1339e065a308ad5819e679cee9919e2b5bc48b34
Lewis1979-oss <lewisimfukwe58@gmail.com>
2026-05-27 10:01:01 +0200

    Fix GitHub build: Force JVM 17 and upgrade Gradle

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,18 +1,3 @@
-import org.gradle.api.tasks.compile.JavaCompile
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
-
-buildscript {
-    repositories {
-        google()
-        mavenCentral()
-    }
-    dependencies {
-        classpath("com.android.tools.build:gradle:8.7.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.21")
-        classpath("com.google.gms:google-services:4.4.2")
-    }
-}
-
 allprojects {
     repositories {
         google()
@@ -20,32 +5,7 @@ allprojects {
     }
 }
 
-rootProject.layout.buildDirectory.value(rootProject.layout.buildDirectory.dir("../../build").get())
-
-subprojects {
-    project.layout.buildDirectory.value(rootProject.layout.buildDirectory.get().dir(project.name))
-    
-    afterEvaluate {
-        if (project.hasProperty("android")) {
-            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
-            android.compileOptions {
-                sourceCompatibility = JavaVersion.VERSION_17
-                targetCompatibility = JavaVersion.VERSION_17
-            }
-        }
-
-        tasks.withType<JavaCompile>().configureEach {
-            sourceCompatibility = "17"
-            targetCompatibility = "17"
-        }
-
-        tasks.withType<KotlinCompile>().configureEach {
-            kotlinOptions {
-                jvmTarget = "17"
-            }
-        }
-    }
-}
+rootProject.layout.buildDirectory.set(rootProject.layout.buildDirectory.dir("../build"))
 
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -6,7 +6,7 @@ pluginManagement {
             if (propertiesFile.exists()) {
                 propertiesFile.inputStream().use { properties.load(it) }
             }
-            properties.getProperty("flutter.sdk") ?: ""
+            properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT") ?: ""
         }
 
     if (flutterSdkPath.isNotEmpty()) {
@@ -24,6 +24,7 @@ plugins {
     id("dev.flutter.flutter-plugin-loader") version "1.0.0"
     id("com.android.application") version "8.7.3" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
 include(":app")
